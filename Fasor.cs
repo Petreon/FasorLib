6e3e900475010c8257d8d1f0e8eaad7fb2fd7a33
Petreon <62783097+Petreon@users.noreply.github.com>
2024-04-29 10:54:33 -0300

    Added files via Upload, without .gitignore visual studio template

---
+++ Fasor.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasor
+{
+    internal class Fasor
+    {
+        #region Properties
+        public double module { get; private set; }
+        public double fase_degree { get; private set; }
+        public double fase_radian { get; private set; }
+        public double real { get; private set; }
+        public double imagy { get; private set; }
+
+        #endregion
+
+        public Fasor(double module, double fase) {
+            this.module = module;
+            this.fase_degree = fase;
+
+            this.fase_radian = Fasor.Degree_to_Radian(this.fase_degree);
+
+            update_rectangular();
+        }
+
+        #region Public_Static
+
+        static public Fasor Sum(Fasor num1, Fasor num2)
+        {
+            double new_real = num1.real + num2.real;
+            double new_imagy = num1.imagy + num2.imagy;
+
+            double real_imagy_ele = Math.Pow(new_real,2) + Math.Pow(new_imagy,2);
+
+            double module = Math.Sqrt(real_imagy_ele);
+            double fase = Math.Atan2(new_imagy, new_real); //THIS FUNCTION RETURN IN RAD ATTENTION
+            fase = Fasor.Radian_to_Degree(fase);
+
+            Fasor sum = new Fasor(module, fase);
+
+            return sum;
+        }
+
+        static public Fasor Minus(Fasor num1, Fasor num2)
+        {
+            double new_real = num1.real - num2.real;
+            double new_imagy = num1.imagy - num2.imagy;
+
+            double real_imagy_ele = Math.Pow(new_real, 2) + Math.Pow(new_imagy, 2);
+
+            double module = Math.Sqrt(real_imagy_ele);
+            double fase = Math.Atan2(new_imagy, new_real); //THIS FUNCTION RETURN IN RAD ATTENTION
+            fase = Fasor.Radian_to_Degree(fase);
+
+
+            Fasor sum = new Fasor(module, fase);
+
+            return sum;
+        }
+
+        static public Fasor Multiply(Fasor num1, Fasor num2)
+        {
+            double new_module = num1.module * num2.module;
+            double new_degree = num1.fase_degree + num2.fase_degree;
+
+            return new Fasor(new_module, new_degree);
+
+        }
+
+        static public Fasor Divide(Fasor num1, Fasor num2)
+        {
+            double new_module = num1.module / num2.module;
+            double new_degree = num1.fase_degree - num2.fase_degree;
+
+            return new Fasor(new_module, new_degree);
+        }
+
+        public static double Degree_to_Radian(double degree)
+        {
+            return degree * (Math.PI / 180);
+        }
+
+        public static double Radian_to_Degree(double degree)
+        {
+            return degree * (180 / Math.PI);
+        }
+
+        #endregion
+
+        #region Public_Methods
+        public void Print_all(string msg=null)
+        {
+            if (msg !=null)
+            {
+                Console.WriteLine($"{msg} Module: {this.module} , Fase: {this.fase_degree} // REC: Real: {this.real} , Imaginary:{this.imagy}");
+
+            }else
+            {
+                Console.WriteLine($"Module: {this.module} , Fase: {this.fase_degree} // REC: Real: {this.real} , Imaginary:{this.imagy}");
+
+            }
+        }
+
+        public void Print(string msg = null)
+        {
+            if (msg != null)
+            {
+                Console.WriteLine($"{msg} Module: {this.module} , Fase: {this.fase_degree}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Module: {this.module} , Fase: {this.fase_degree}");
+
+            }
+        }
+
+
+        #endregion
+
+        #region Private_Methods
+
+        private void update_rectangular()
+        {
+            this.real = this.module * Math.Cos(this.fase_radian);
+            this.imagy = this.module * Math.Sin(this.fase_radian);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Fasor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // constants
+            Fasor R1 = new Fasor(2200, 0);
+            Fasor R2 = new Fasor(1000, 0);
+            Fasor Vt = new Fasor(3, 0);
+            double[] frequency = { 100, 500, 1000, 2000, 10000 };
+            double c11_constant = 0.01e-6;//microfarad
+            double c15_constant = 0.05e-6;
+            double c2_constant = 0.022e-6;
+
+
+
+            List<Fasor> C11_list = new List<Fasor>();
+            List<Fasor> C15_list = new List<Fasor>();
+            List<Fasor> C2_list = new List<Fasor>();
+
+
+
+            //initialize the capacitors
+            for(int i = 0; i < 5; i++)
+            {
+                double XC11_absolute = 1 / (2 * Math.PI * frequency[i] * c11_constant);
+                Fasor XC11 = new Fasor(XC11_absolute, -90);
+                C11_list.Add(XC11);
+
+                double XC15_absolute = 1 / (2 * Math.PI * frequency[i] * c15_constant);
+                Fasor XC15 = new Fasor(XC15_absolute, -90);
+                C15_list.Add(XC15);
+
+                double XC2_absolute = 1 / (2 * Math.PI * frequency[i] * c2_constant);
+                Fasor XC2 = new Fasor(XC2_absolute, -90);
+                C2_list.Add(XC2);
+            }
+
+            List<Fasor> Z11Req_list = new List<Fasor>();
+            List<Fasor> Z15Req_list = new List<Fasor>();
+            List<Fasor> Z2Req_list = new List<Fasor>();
+
+            for(int i = 0; i < 5;i++)
+            {
+                Fasor R1_C11 = Parallel(R1, C11_list[i]);
+                //R1_C11.Print($"Z11 eq: {frequency[i]}Hz");
+                Fasor R1_C15 = Parallel(R1, C15_list[i]);
+                //R1_C15.Print($"Z15 eq: {frequency[i]}Hz");
+                Fasor R2_C2 = Parallel(R2, C2_list[i]);
+                //R2_C2.Print($"Z2 eq: {frequency[i]}Hz");
+
+                Z11Req_list.Add(R1_C11);
+                Z15Req_list.Add(R1_C15);
+                Z2Req_list.Add(R2_C2);
+
+            }
+
+            List<Fasor> V0_list = new List<Fasor>();
+
+            for(int i = 0; i < 5;i++)
+            {
+                Fasor Queda_V = Tension_Divisor(Z2Req_list[i], Z11Req_list[i], Vt);
+                V0_list.Add(Queda_V);
+                V0_list[i].Print($"V0 em C1 = 0.01uF; Frequencia = {frequency[i]}Hz");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Fasor Queda_V = Tension_Divisor(Z2Req_list[i], Z15Req_list[i], Vt);
+                V0_list.Add(Queda_V);
+                V0_list[i+5].Print($"V0 em C1 = 0.05uF; Frequencia = {frequency[i]}Hz");
+
+            }
+        }
+
+        public static Fasor Parallel(Fasor num1, Fasor num2)
+        {
+            Fasor upside = Fasor.Multiply(num1, num2);
+            Fasor downside = Fasor.Sum(num1, num2);
+            Fasor result = Fasor.Divide(upside, downside);
+
+            return new Fasor(result.module, result.fase_degree);
+        }
+
+        public static Fasor Tension_Divisor(Fasor Z1, Fasor Z2, Fasor Tension)
+        {
+
+            Fasor upside = Z1;
+            Fasor downside = Fasor.Sum(Z1, Z2);
+            Fasor result1 = Fasor.Divide(upside, downside);
+            Fasor result2 = Fasor.Multiply(result1, Tension);
+
+            return new Fasor(result2.module, result2.fase_degree);
+
+        }
+    }
+}
